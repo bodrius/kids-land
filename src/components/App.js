5879688e571e3eea40b93fc8d817cbd6fc3fe19f
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-04-23 17:05:14 +0300

    add auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,16 @@
-import React, { useState, useEffect } from "react";
-// import { WeekTabs } from "./main/WeekTabs";
-// import CardListUl from "./cardList/CardListUl";
-import { services } from "../services/services";
+import React from "react";
+import { HeaderRouting } from "../components/headerRouting/HeaderRouting";
+import { useSelector } from "react-redux";
 import { Header } from "./header/Header";
 
-// import { AwardsPage } from "./awardsPage/AwardsPage";
-import { HeaderRouting } from "../components/headerRouting/HeaderRouting";
-import { MainPage } from "./mainPage/Mainpage";
-import Auth from "./auth/Auth";
-import { useSelector } from "react-redux";
-
-export const App = (props) => {
-  const routing = HeaderRouting();
-
-  // const token = useSelector((state) => state.user.userToken);
- 
-  
-  // useEffect(() => {
-  //   if(token){
-  //     props.history.replace("/")
-  //   }
-   
-  // }, []);
-
-// console.log(props.history.replace("/"))
-  
-
-  // services.createUser({email: "test1234@gmail.com", password: "qwertys"}).then(data=>console.log('signUp', data));
-
-  // services
-  //   .userSignIn({ email: "test1234@gmail.com",WeekTabs password: "qwertys" })
-  //   .then((data) => console.log("signIn", data));
-
-  // services.getCurrentUser("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOWY1Zjk2MWU0NDY3NWYwNGFjMGZkNCIsImlhdCI6MTU4NzUwMzQxMCwiZXhwIjoxNTg4MTA4MjEwfQ.YCQctkw76xPB6uv9RsoMae_MsTEVQb1huaXKrfkqHzk").then(data=>console.log('currentUser', data));
-
-  // services.updateUserPoints("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOWY1Zjk2MWU0NDY3NWYwNGFjMGZkNCIsImlhdCI6MTU4NzUwMzQxMCwiZXhwIjoxNTg4MTA4MjEwfQ.YCQctkw76xPB6uv9RsoMae_MsTEVQb1huaXKrfkqHzk",'5e9f5f961e44675f04ac0fd4',100).then(data=>console.log('currentUser', data));
-
-  // services.createUserTask("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOWY1Zjk2MWU0NDY3NWYwNGFjMGZkNCIsImlhdCI6MTU4NzUwMzQxMCwiZXhwIjoxNTg4MTA4MjEwfQ.YCQctkw76xPB6uv9RsoMae_MsTEVQb1huaXKrfkqHzk",{title: 'Task title', taskPoints: 10}).then(data=>console.log('currentUser', data));
-
-  services
-    .updateUserTask(
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOWY1Zjk2MWU0NDY3NWYwNGFjMGZkNCIsImlhdCI6MTU4NzUwMzQxMCwiZXhwIjoxNTg4MTA4MjEwfQ.YCQctkw76xPB6uv9RsoMae_MsTEVQb1huaXKrfkqHzk",
-      "5e9f6dee1e44675f04ac0fde",
-      { title: "New title" }
-    )
-    .then((data) => console.log("currentUser", data));
+export const App = () => {
+  const token = useSelector((state) => state.user.userToken);
+  const routing = HeaderRouting(token);
 
   return (
     <>
-      {/* <Header /> */}
-       <Auth />
-       
-        
-        {/* <WeekTabs /> */}
-        {/* <CardListUl /> */}
-        {/* <AwardsPage /> */}
-      {/* <MainPage/> */}
-      {/* {routing} */}
+      <Header token={token} />
+      {routing}
     </>
   );
 };
--- src/components/headerRouting/HeaderRouting.js
+++ src/components/headerRouting/HeaderRouting.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line
 import React, { Suspense, lazy } from "react";
-import { Route, Switch } from "react-router-dom";
-// import {LoaderUi} from "../ui/loader/Loader";
+import { Route, Switch, Redirect } from "react-router-dom";
+import {LoaderUi} from "../ui/loader/Loader";
 import { MainPage } from "../mainPage/Mainpage";
 import { AwardsPage } from "../awardsPage/AwardsPage";
-import { NotFound } from "../pageNoteFound/NotFound";
+import Auth from "../auth/Auth"
 
 // const MainPage = lazy(() =>
 //   import("../mainPage/Mainpage" /* webpackChunkName: 'MainPage'*/)
@@ -21,19 +21,18 @@ import { NotFound } from "../pageNoteFound/NotFound";
 //   import("../pageNoteFound/NotFound" /* webpackChunkName: 'pageNoteFound'*/)
 // );import { NotFound } from '../pageNoteFound/NotFound';
 
-export const HeaderRouting = () => {
-  return (
-    <>
-      {/* <Suspense fallback={<LoaderUi />}> */}
-      {/* <Suspense fallback={<div>LOADING</div>}> */}
-        <Switch>
-          {/* <Route exact path="/" component={MainPage} /> */}
-          {/* <Route path="/planning" component={PlanningPage} /> */}
-          <Route path="/awards" component={AwardsPage} />
-          {/* <Route path="/contacts-us" component={ContactsPage} /> */}
-          {/* <Route component={NotFound} /> */}
-        </Switch>
-      {/* </Suspense> */}
-    </>
-  );
+
+export const HeaderRouting = (token) => {
+  if(token){
+    return <Suspense fallback={<LoaderUi/>}>
+    <Switch>
+      <Route exact path="/" component={MainPage} /> 
+      {/* <Route path="/planning" component={PlanningPage} />  */}
+      <Route path="/awards" component={AwardsPage} /> 
+      {/* <Route path="/contacts-us" component={ContactsPage} /> */}
+      <Redirect to='/'/>
+    </Switch>
+    </Suspense>
+  }
+  return <Auth/>
 };
