46d951c0501867a1d3cf8f92c2cc57aac43b0584
Serhii-Zuiev <walter94walter@gmail.com>
2020-04-24 12:04:17 +0300

    addCustomTaskMethods

--- src/components/planningPage/PlanningPage.js
+++ src/components/planningPage/PlanningPage.js
@@ -4,10 +4,12 @@ import data from "../../common/cardList/data.json";
 import {PlanningPoints} from '../PlanningPoints/PlanningPoints'
 import {AddCustomTask} from '../AddCustomTask/AddCustomTask'
 import { Footer } from '../Footer/Footer'
+import { NewTaskModal } from "../newTaskModal/NewTaskModal";
 
 export const PlanningPage = () => {
   const {user: {tasks}} = data;
   const [allUserPoints, setAllUserPoints] = React.useState(0);
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
   const total = tasks.reduce((prev, current) => {
     const days = current.days.filter((day) => day.isActive === true).length;
     return prev + current.taskPoints * days;
@@ -18,10 +20,16 @@ export const PlanningPage = () => {
     setAllUserPoints(allUserPoints + p);
   };
   console.log('allUserPoints', allUserPoints)
+
+function handleOpenTaskModal() {
+    setModalIsOpen(true);
+  }
+
   return (
     <>
+    {modalIsOpen && <NewTaskModal/>}
     <PlanningPoints />
-    <AddCustomTask />
+    <AddCustomTask handleOpenTaskModal={handleOpenTaskModal}/>
     <CardList
       plusPoint={plusPoint}
       data={tasks}
