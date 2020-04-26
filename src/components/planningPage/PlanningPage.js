a4a769f379232a9f725b344554a36c698aacb649
gennadiy123 <shelukhin_g@ukr.net>
2020-04-26 13:24:04 +0300

    merging

--- src/components/planningPage/PlanningPage.js
+++ src/components/planningPage/PlanningPage.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { CardList } from "../../common/cardList/CardList";
 import data from "../../common/cardList/data.json";
-import {PlanningPoints} from '../PlanningPoints/PlanningPoints'
-import {AddCustomTask} from '../AddCustomTask/AddCustomTask'
-import { Footer } from '../Footer/Footer'
+import { PlanningPoints } from "../PlanningPoints/PlanningPoints";
+import { AddCustomTask } from "../AddCustomTask/AddCustomTask";
+import { Footer } from "../Footer/Footer";
 import { NewTaskModal } from "../newTaskModal/NewTaskModal";
-import {services} from "../../services/services"
+import { services } from "../../services/services";
 
- const PlanningPage = () => {
-  const {user: {tasks}} = data;
+const PlanningPage = () => {
+  const [tasks, setTasks] = React.useState(data.user.tasks);
   const [allUserPoints, setAllUserPoints] = React.useState(0);
   const [modalIsOpen, setModalIsOpen] = React.useState(false);
   const [customTask, setCustomTask] = React.useState({});
@@ -23,47 +23,51 @@ import {services} from "../../services/services"
     console.log("p", p);
     setAllUserPoints(allUserPoints + p);
   };
-  console.log('allUserPoints', allUserPoints)
+  console.log("allUserPoints", allUserPoints);
 
   function handleOpenTaskModal() {
     setModalIsOpen(true);
   }
 
-  function handleCloseTaskModal () {
+  function handleCloseTaskModal() {
     setModalIsOpen(false);
   }
 
-  function handleCollectCustomTask (task) {
+  function handleCollectCustomTask(task) {
     setCustomTask(task);
-
   }
 
-  async function answerFromServer (token, customTask)  {
-    const answer = await services.createUserTask(token, customTask);
-    
-    console.log('answer', answer)
+  async function answerFromServer(token, customTask) {
+    const answer = await services
+      .createUserTask(token, customTask)
+      .then(({data: {tasks}, status}) => {
+        status === 200 && console.log('tasks', tasks.map((task) => {
+          return `${task.title}, ${task.imgName}`
+        }))
+      });
+
+    console.log("answer", answer);
   }
 
   React.useEffect(() => {
-  // console.log('customTask', customTask)
-  answerFromServer(token, customTask)
-  }, [customTask])
-
+    // console.log('customTask', customTask)
+    answerFromServer(token, customTask);
+  }, [customTask]);
 
   return (
     <>
-    {modalIsOpen && <NewTaskModal onClose={handleCloseTaskModal} onHandleCollectCustomTask = {handleCollectCustomTask}/>}
-    <PlanningPoints />
-    <AddCustomTask handleOpenTaskModal={handleOpenTaskModal}/>
-    <CardList
-      plusPoint={plusPoint}
-      data={tasks}
-      
-    />
-    <Footer/>
+      {modalIsOpen && (
+        <NewTaskModal
+          onClose={handleCloseTaskModal}
+          onHandleCollectCustomTask={handleCollectCustomTask}
+        />
+      )}
+      <PlanningPoints />
+      <AddCustomTask handleOpenTaskModal={handleOpenTaskModal} />
+      <CardList plusPoint={plusPoint} data={tasks} />
+      <Footer />
     </>
   );
 };
-
 
 export default PlanningPage;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const deleteContact = createAction("ADD_CARD");
 
