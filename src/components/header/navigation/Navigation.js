6a3ab058d87fae11a5dea1b34be4094ac451d92b
Sergei <xsergeix@ukr.net>
2020-04-27 13:19:15 +0300

    header-burger-logic

--- src/components/header/navigation/Navigation.js
+++ src/components/header/navigation/Navigation.js
@@ -12,24 +12,55 @@ export const Navigation = () => {
       {window.screen.width < 1199 && isMenuOpen && (
         <>
           <div className={css.burgerContainer}>
+            <div className={css.overlay_2}>
             <div className={css.overlay}>
               <div className={css.pages}>
-                <NavLink exact  to='/'  activeStyle={{color:'black'}}
-                style={{color:' #e0e0e5'}} className={css.text}>Головна сторінка</NavLink>
+                <NavLink
+                  onClick={openModal}
+                  exact
+                  to="/"
+                  activeStyle={{ color: "black" }}
+                  style={{ color: " #e0e0e5" }}
+                  className={css.text}
+                >
+                  Головна сторінка
+                </NavLink>
               </div>
               <div className={css.pages}>
-                <NavLink to='planning'   activeStyle={{color:'black'}}
-                style={{color:' #e0e0e5'}} className={css.text}>Планування</NavLink>
+                <NavLink
+                onClick={openModal}
+                  to="planning"
+                  activeStyle={{ color: "black" }}
+                  style={{ color: " #e0e0e5" }}
+                  className={css.text}
+                >
+                  Планування
+                </NavLink>
               </div>
               <div className={css.pages}>
-                <NavLink to='awards' activeStyle={{color:'black'}}
-                style={{color:' #e0e0e5'}} className={css.text}>Призи</NavLink>
+                <NavLink
+                  onClick={openModal}
+                  to="awards"
+                  activeStyle={{ color: "black" }}
+                  style={{ color: " #e0e0e5" }}
+                  className={css.text}
+                >
+                  Призи
+                </NavLink>
               </div>
               <div className={css.pages}>
-                <NavLink to ='contact-us' activeStyle={{color:'black'}}
-                style={{color:' #e0e0e5'}} className={css.text}>Контакти</NavLink>
+                <NavLink
+                  onClick={openModal}
+                  to="contact-us"
+                  activeStyle={{ color: "black" }}
+                  style={{ color: " #e0e0e5" }}
+                  className={css.text}
+                >
+                  Контакти
+                </NavLink>
               </div>
               <button className={css.closeModal} onClick={openModal}></button>
+            </div>
             </div>
           </div>
         </>
@@ -46,18 +77,46 @@ export const Navigation = () => {
       {window.screen.width >= 1200 && (
         <>
           <ul className={css.pageList}>
-            <li className={css.pageItem}><NavLink exact to='/' activeStyle={{color:'black'}}
-                style={{color: '#a3a3b1'}} >Головна сторінка</NavLink></li>
-            <li className={css.pageItem}><NavLink to='/planning' activeStyle={{color:'black'}}
-                style={{color:' #a3a3b1'}} >Планування</NavLink></li>
-            <li className={css.pageItem}><NavLink to='/awards'  activeStyle={{color:'black'}}
-                style={{color:' #a3a3b1'}} >Призи</NavLink></li>
-            <li className={css.pageItem}><NavLink to='/contact-us'  activeStyle={{color:'black'}}
-                style={{color:' #a3a3b1'}} >Контакти</NavLink></li>
+            <li className={css.pageItem}>
+              <NavLink
+                exact
+                to="/"
+                activeStyle={{ color: "black" }}
+                style={{ color: "#a3a3b1" }}
+              >
+                Головна сторінка
+              </NavLink>
+            </li>
+            <li className={css.pageItem}>
+              <NavLink
+                to="/planning"
+                activeStyle={{ color: "black" }}
+                style={{ color: " #a3a3b1" }}
+              >
+                Планування
+              </NavLink>
+            </li>
+            <li className={css.pageItem}>
+              <NavLink
+                to="/awards"
+                activeStyle={{ color: "black" }}
+                style={{ color: " #a3a3b1" }}
+              >
+                Призи
+              </NavLink>
+            </li>
+            <li className={css.pageItem}>
+              <NavLink
+                to="/contact-us"
+                activeStyle={{ color: "black" }}
+                style={{ color: " #a3a3b1" }}
+              >
+                Контакти
+              </NavLink>
+            </li>
           </ul>
         </>
       )}
     </>
   );
 };
-
