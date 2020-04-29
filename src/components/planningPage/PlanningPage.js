54c2fc33e2be6ca2aa8bb2012136016431fe996e
gennadiy123 <shelukhin_g@ukr.net>
2020-04-29 16:09:07 +0300

    before PlanningPoints

--- src/components/planningPage/PlanningPage.js
+++ src/components/planningPage/PlanningPage.js
@@ -14,20 +14,14 @@ const PlanningPage = () => {
   const [tasks, setTasks] = React.useState([]);
   const [allUserPoints, setAllUserPoints] = React.useState(0);
   const [modalIsOpen, setModalIsOpen] = React.useState(false);
-  // const [customTask, setCustomTask] = React.useState({});
   const userToken = useSelector((state) => state.user.userToken);
-  // console.log("userToken", userToken);
-  // console.log("tasks", tasks);
   const total = tasks.reduce((prev, current) => {
     const days = current.days.filter((day) => day.isActive === true).length;
     return prev + current.taskPoints * days;
   }, 0);
-  // console.log("total", total);
   const plusPoint = (p) => {
-    // console.log("p", p);
     setAllUserPoints(allUserPoints + p);
   };
-  // console.log('allUserPoints', allUserPoints)
 
   function handleOpenTaskModal() {
     setModalIsOpen(true);
@@ -41,39 +35,7 @@ const PlanningPage = () => {
     services.createUserTask(userToken, task).then(({ data: { tasks } }) => {
       setTasks(tasks);
     });
-    // setTasks([...tasks, task]);
-    // setCustomTask(task);
   }
-
-  // async function answerFromServer(userToken, customTask) {
-  //   const answer = await services
-  // .createUserTask(userToken, customTask)
-
-  // .then(({data: {tasks}, status}) => {
-  //   status === 200 && console.log('tasks', tasks.map((task) => {
-  //     return `${task.title}, ${task.imgName}`
-  //   }))
-  // });
-
-  // .then((data) => data.status.ok && action для записи в стор)
-
-  // .then(({ data: { tasks }, status }) => {
-  //   status === 200 &&
-  //     console.log(
-  //       "tasks",
-  //       tasks.map((task) => {
-  //         return `${task.title}, ${task.imgName}`;
-  //       })
-  //     );
-  // });
-
-  // await services
-  //   .getCurrentUser(userToken)
-  //   .then((data) => setTasks(data.data.user.tasks));
-  // }
-
-  // иф статус ОК - добавить в стор
-  // обновить значение в сторе
 
   const getTasks = () => {
     services.getCurrentUser(userToken).then(({ data: { user } }) => {
