512b3f39fd9c4c036af19228f20171cfb03cb00f
Karlson <m.norets@gmail.com>
2020-04-30 17:14:15 +0300

    add plus done task

--- src/components/cardList/cardListLi/CardListLi.js
+++ src/components/cardList/cardListLi/CardListLi.js
@@ -6,7 +6,7 @@ import ButtonGood from "../../../common/bottonGod/ButtonGood";
 // import ButtonBad from "../../../common/buttonBad/ButtonBad";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-
+import { services } from "../../../services/services";
 const date = moment().format("Do MMMM YYYY");
 
 const CardListLi = ({
@@ -18,7 +18,8 @@ const CardListLi = ({
   location,
   dayLabel,
 }) => {
-  const { userToken } = useSelector((state) => state.user);
+  const { userToken, userId } = useSelector((state) => state.user);
+
   // const drawing = () => {
   //   if (location.pathname === "/awards") {
   //     return (
@@ -87,6 +88,7 @@ const CardListLi = ({
                   }}
                   className={style.Card__listBtton}
                   onClick={async () => {
+                    console.log("list", list);
                     const chooseDays = await currentTask(list.id, userToken);
                     await updateСommission(
                       list.days[0][0].title,
@@ -94,12 +96,15 @@ const CardListLi = ({
                       userToken,
                       chooseDays
                     );
+                    const allPoints = await completedtask(userToken);
+                    console.log("allpoints", allPoints);
+                    await addPoints(userId, userToken, allPoints, list.points);
                   }}
                 >
                   +
                 </button>
               ) : (
-                <ButtonGood/>
+                <ButtonGood />
               )}
             </>
           )}
@@ -107,6 +112,24 @@ const CardListLi = ({
       </div>
     </li>
   );
+};
+
+const addPoints = async (id, token, points, cardPoint) => {
+  const updatePoint = points + cardPoint;
+  await services.updateUserPoints(token, id, updatePoint);
+};
+
+const completedtask = async (token) => {
+  const allpoints = await axios.get(
+    "https://kidslike.goit.co.ua/api/auth/current",
+    {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return allpoints.data.user.points;
 };
 
 const currentTask = async (id, token) => {
